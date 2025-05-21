ceb1344346ce46e53ecb777805296d812b9d8e4f
Antonio Felix de Oliveira Neto <antoniofelixxx7@gmail.com>
2025-05-21 18:15:14 -0300

    Só fazendo a apresentação

---
+++ apresentacao.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("\n\t\t  ------------                               ------------------------------------------- \n");
+    printf("\t\t | Bom Dia !! |                             | Essa apresentação vai ser do balacobaco!! |\n"); 
+    printf("  \t\t /-------------                            / -------------------------------------------  \n");
+    printf("    *****\t/                              *****      /\n");
+    printf("  *       *    /\t                     *       *   /\n");
+    printf(" *  O   O  *  /                      \t    *  -   o  * /\n");
+    printf("*     ^     *                              *     ~     *\n");
+    printf("*   \\___/   *                              *  \\_v-v_/  *\n");
+    printf(" *         *                                *          * \n");
+    printf("  *       *                                  *        *  \n");
+    printf("    *****                                      *****         \n");
+
+    printf("\n\nVamos aprender um pouco sobre: \n[1.] - Git\n[2.] - GitHub\n[3.] - GitLab\n");
+
+    return 0;
+}
+
